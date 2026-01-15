131535c0cb37f48d923add5abeed82272277d5fd
Arjit <129994490+Arjit31@users.noreply.github.com>
2026-01-15 15:53:30 +0530

    Add new project 'Workflow' to Works component

--- components/Works.jsx
+++ components/Works.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const data = [
+  { title: "Workflow", image: "./img/workflow.png", link: "https://zap-frontend-y17p.vercel.app/" },
   { title: "Meet & Code", image: "./img/code-along.png", link: "https://code-along-client.netlify.app/" },
   { title: "Wallet Application", image: "./img/wallet.png", link: "https://github.com/Arjit31/wallet-monorepo" },
   { title: "Chat Application", image: "./img/chat.png", link: "https://github.com/Arjit31/chat-app" },
   { title: "Tech Blog", image: "./img/TechBlog.png", link: "https://medium-client-snowy.vercel.app/" },
-  { title: "Connection Game", image: "./img/connectionGame.jpeg", link: "https://gamefront.onrender.com/" },
-  { title: "Keeper App", image: "./img/keeper.jpg", link: "https://64d36e000fc1a14f7a431f74--remarkable-croquembouche-4c7402.netlify.app/" },
+  { title: "Connection Game", image: "./img/connectionGame.jpeg", link: "https://gamefront.onrender.com/" }
 ];
 
 const Section = styled.div`
